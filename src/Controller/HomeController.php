eb04185e72579f641b35b0283698592936b28e8e
Antoine <sirpa.antoine@gmail.com>
2025-11-02 21:13:35 +0000

    test

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,15 +2,21 @@
 
 namespace App\Controller;
 
+use App\Repository\UserRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class HomeController extends AbstractController
 {
+    public function __construct(private UserRepository $userRepository) {}
+
     #[Route('/', name: 'app_home')]
     public function index(): Response
     {
+        $users = $this->userRepository->findAll();
+        var_dump($users);die;
+
         return $this->render('home/index.html.twig', [
             'controller_name' => 'HomeController',
         ]);
--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -1,16 +1,30 @@
 <?php
 
+declare(strict_types=1);
+
 namespace App\DataFixtures;
 
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Faker\Factory;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
+        $faker = Factory::create('fr_FR');
+
+        for ($i = 0; $i < 5; $i++) {
+            $user = new User();
+
+            $user->setName($faker->unique()->name());
+            $user->setEmail($faker->unique()->email());
+            // $user->setPassword($passworHasher->hashPassword($user, 'qwerty'));
+
+            $manager->persist($user);
+        }
 
         $manager->flush();
     }
